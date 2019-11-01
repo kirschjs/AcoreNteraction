new (monotonous) LEC and oscillator-strength fits; HOMPJM.py notebook added with correct sign relation between V1 and V234 (non-local)

HOMPJM.py
@@ -14,25 +14,6 @@ from sympy.physics.quantum.cg import CG, Wigner3j
 import numpy.ma as ma
 
 ###############################
-######## V LO pionless ########
-###############################
-lambdas = [
-    0.05, 0.1, 0.16, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7,
-    0.75, 0.8, 0.9, 0.95, 1, 1.05, 1.1, 1.2, 1.5, 2, 3, 4, 6, 10
-]
-LeCofL = [
-    -1.552, -2.242, -3.440, -4.040, -6.397, -7.808, -9.310, -10.976, -12.782,
-    -14.726, -16.809, -19.032, -21.394, -23.894, -26.536, -32.234, -35.291,
-    -38.489, -41.824, -45.299, -52.666, -78.111, -131.646, -280.457, -484.921,
-    -1060.797, -2880.387
-]
-LeDofL = [
-    -0.324, -0.246, 0.019, 0.223, 1.207, 1.981, 2.828, 3.913, 5.209, 6.718,
-    8.468, 10.463, 12.734, 15.276, 18.137, 24.813, 28.664, 32.911, 37.527,
-    42.570, 53.971, 100.476, 232.413, 854.107, 2495.364, 16915.630, 756723.220
-]
-
-###############################
 ######## Wave function ########
 ###############################
 
@@ -83,37 +64,55 @@ def ddpsi(r, n, l, nu):
 pedantic = 0
 states_print = 3  # How many energies do you want?
 
-# Parallel of this version not implemented
-parallel = False  # Do you want trallel version?
-Nprocessors = 6  # Number of processors
-
 mpi = '137'
 m = 938.12
 hbar = 197.327
+
 # Prague-Jerusalem-Manchester effective A-1 interaction
 
-# number of core particles (capital A in docu)
+# array which holds the critical Lambdas
 Lc = []
 
-for Ncore in range(16, 20):
+# define the range of core numbers
+Amin = 7
+Amax = 35
+cores = range(Amin, Amax)
 
-    Lamb = Lc[-1][1] if Lc != [] else 1.0
-    Lamb = 10.0
+# for each core number, determine an oscillator strength which
+# fixes the size of this S-wave core
+# [2] : volume formula [1] : polynomial with + powers [0] : polynomial with +/- powers
+coreoscis = fita(cores, order=3, orderp=1, plot=0)[2]
+print(coreoscis)
+# select a cutoff range in which the critical value is sought
+Lmin = 0.1
+Lmax = 78.0
+dL = 1.0
+Lrange = np.arange(Lmin, Lmax, dL)
+
+parametriz = "C1_D4"
+LeCmodel = return_val(Lrange, parametriz, "C", polord=3, plot=0)
+LeDmodel = return_val(Lrange, parametriz, "D", polord=4, plot=0)
+
+for Ncore in cores:
+
+    # assume that lc is larger for a larger core, and thus begin searching
+    # for an unstable system from the lc of the Ncore-1 value
     stable = True
+    Lamb = Lc[-1][1] if Lc != [] else 0.84
+
     while (stable):
-        NState = 20
-        Rmax = 20
-        order = 300
-        omegas = np.linspace(0.01, 0.5, 10)
+        NState = 35
+        Rmax = 30
+        order = 400
+        omegas = np.linspace(0.05, 0.25, 4)
         #omegas = [1.5]
 
         L = 1
 
-        parametriz = "C1_D4"
-        LeC = return_val(Lamb, parametriz, "C")
-        LeD = return_val(Lamb, parametriz, "D")
+        LeC = polyval(Lamb, LeCmodel.x)
+        LeD = polyval(Lamb, LeDmodel.x)
 
-        coreosci = fita(Ncore, plot=True)
+        coreosci = coreoscis[Ncore - Amin]
 
         mu = Ncore * m / (Ncore + 1.)
         mh2 = hbar**2 / (2 * mu)
@@ -121,7 +120,7 @@ for Ncore in range(16, 20):
         potargs = [coreosci, Ncore, float(Lamb), LeC, LeD]
 
         interaction = "NonLocal"
-        energydepen = False
+        energydepen = True
 
         aa1 = alf1(potargs)
         aa2 = alf2(potargs)
@@ -154,40 +153,22 @@ for Ncore in range(16, 20):
         wigm = 0 if (w3j_m2 == 0) else float(w3j_m2.evalf())
         wigp = 0 if (w3j_p2 == 0) else float(w3j_p2.evalf())
 
-        print(" - RGM potential -")
-        print("Lambda  = " + str(Lamb))
-        print("Ncore   = " + str(Ncore))
-        print("a core  = " + str(coreosci))
+        print("Lambda  = %2.2f  A = %d\n" % (Lamb, Ncore))
+        print("a core  = %4.4f\n" % coreosci)
+        print("LEC 2b = %+6.4e   LEC 3b  = %+6.4e\n" % (LeC, LeD))
+        print('            1            2            3            4')
+        print("alpha  = %+6.4e    %+6.4e    %+6.4e    %+6.4e" % (aa1, aa2, aa3,
+                                                                 aa4))
+        print("beta   = %+6.4e    %+6.4e    %+6.4e    %+6.4e" % (bb1, bb2, bb3,
+                                                                 bb4))
+        print("gamma  = %+6.4e    %+6.4e    %+6.4e    %+6.4e" % (gg1, gg2, gg3,
+                                                                 gg4))
+        print("kappa  = %+6.4e    %+6.4e    %+6.4e\n" % (kk1, kk2, kk3))
+        print("eta    = %+6.4e    %+6.4e    %+6.4e" % (nn1, nn2, nn3))
+        print("zeta   = %+6.4e    %+6.4e    %+6.4e    %+6.4e\n" % (zz1, zz2,
+                                                                   zz3, zz4))
         if pedantic:
-            print("LEC 2b  = " + str(LeC))
-            print("LEC 3b  = " + str(LeD))
-            print(" -- ")
-            print("alpha1  = " + str(aa1))
-            print("alpha2  = " + str(aa2))
-            print("alpha3  = " + str(aa3))
-            print("alpha4  = " + str(aa4))
-            print("beta1   = " + str(bb1))
-            print("beta2   = " + str(bb2))
-            print("beta3   = " + str(bb3))
-            print("beta4   = " + str(bb4))
-            print("gamma1  = " + str(gg1))
-            print("gamma2  = " + str(gg2))
-            print("gamma3  = " + str(gg3))
-            print("gamma4  = " + str(gg4))
-            print("zeta1   = " + str(zz1))
-            print("zeta2   = " + str(zz2))
-            print("zeta3   = " + str(zz3))
-            print("zeta4   = " + str(zz4))
-            print("eta1    = " + str(nn1))
-            print("eta2    = " + str(nn2))
-            print("eta3    = " + str(nn3))
-            print("kappa1  = " + str(kk1))
-            print("kappa2  = " + str(kk2))
-            print("kappa3  = " + str(kk3))
-            print(" -- ")
             print("W3J\'s  = " + str(w3j_p2) + "  " + str(w3j_m2))
-            print(" -- ")
-            exit()
 
         def exchange_kernel(rl, rr, argv):
 
@@ -199,7 +180,11 @@ for Ncore in range(16, 20):
                 spherical_jn(L, 1j * bb1 * rr * rl),
                 nan=0.0,
                 posinf=0.0,
-                neginf=0.0) * np.exp(-aa1 * rr2 - gg1 * rl2)))
+                neginf=0.0) * np.nan_to_num(
+                    np.exp(-aa1 * rr2 - gg1 * rl2),
+                    nan=0.0,
+                    posinf=0.0,
+                    neginf=0.0)))
 
             return np.nan_to_num(np.real(V1ex))
 
@@ -208,46 +193,58 @@ for Ncore in range(16, 20):
             rr2 = rr**2
             rl2 = rl**2
 
-            V1 = -mh2 * zz1 * np.exp(-aa1 * rr2 - gg1 * rl2) * (
-                4. * aa1 * bb1 * rl * rr *
-                ((1j**
-                  (L - 1) * np.nan_to_num(
-                      spherical_jn(L - 1, 1j * bb1 * rr * rl),
-                      nan=0.0,
-                      posinf=0.0,
-                      neginf=0.0) * wigm * (2 * L - 3) + 1j**
-                  (L + 1) * np.nan_to_num(
-                      spherical_jn(L + 1, 1j * bb1 * rr * rl),
-                      nan=0.0,
-                      posinf=0.0,
-                      neginf=0.0) * wigp * (2 * L - 1)) +
-                 (-4. * aa1**2 * rr2 + 2 * aa1 - bb1**2 * rl2 + L *
-                  (L + 1) / rr2) * 1j**L * np.nan_to_num(
-                      spherical_jn(L, 1j * bb1 * rr * rl),
-                      nan=0.0,
-                      posinf=0.0,
-                      neginf=0.0)))
+            V1 = -mh2 * zz1 * np.nan_to_num(
+                np.exp(-aa1 * rr2 - gg1 * rl2),
+                nan=0.0,
+                posinf=0.0,
+                neginf=0.0) * (4. * aa1 * bb1 * rl * rr * (
+                    (1j**
+                     (L - 1) * np.nan_to_num(
+                         spherical_jn(L - 1, 1j * bb1 * rr * rl),
+                         nan=0.0,
+                         posinf=0.0,
+                         neginf=0.0) * wigm * (2 * L - 3) + 1j**
+                     (L + 1) * np.nan_to_num(
+                         spherical_jn(L + 1, 1j * bb1 * rr * rl),
+                         nan=0.0,
+                         posinf=0.0,
+                         neginf=0.0) * wigp * (2 * L - 1)) +
+                    (-4. * aa1**2 * rr2 + 2 * aa1 - bb1**2 * rl2 + L *
+                     (L + 1) / rr2) * 1j**L * np.nan_to_num(
+                         spherical_jn(L, 1j * bb1 * rr * rl),
+                         nan=0.0,
+                         posinf=0.0,
+                         neginf=0.0)))
 
             # this is simple (still missing (4 pi i^l ) RR')
-            V234 = -(1j**L) * (
-                (zz2 * np.nan_to_num(
-                    spherical_jn(L, 1j * bb2 * rr * rl),
+            V234 = -(1j**L) * ((zz2 * np.nan_to_num(
+                spherical_jn(L, 1j * bb2 * rr * rl),
+                nan=0.0,
+                posinf=0.0,
+                neginf=0.0) * np.nan_to_num(
+                    np.exp(-aa2 * rr2 - gg2 * rl2),
                     nan=0.0,
                     posinf=0.0,
-                    neginf=0.0) * np.exp(-aa2 * rr2 - gg2 * rl2)) +
-                (zz3 * np.nan_to_num(
-                    spherical_jn(L, 1j * bb3 * rr * rl),
-                    nan=0.0,
-                    posinf=0.0,
-                    neginf=0.0) * np.exp(-aa3 * rr2 - gg3 * rl2)) +
-                (zz4 * np.nan_to_num(
-                    spherical_jn(L, 1j * bb4 * rr * rl),
-                    nan=0.0,
-                    posinf=0.0,
-                    neginf=0.0) * np.exp(-aa4 * rr2 - gg4 * rl2)))
+                    neginf=0.0)) + (zz3 * np.nan_to_num(
+                        spherical_jn(L, 1j * bb3 * rr * rl),
+                        nan=0.0,
+                        posinf=0.0,
+                        neginf=0.0) * np.nan_to_num(
+                            np.exp(-aa3 * rr2 - gg3 * rl2),
+                            nan=0.0,
+                            posinf=0.0,
+                            neginf=0.0)) + (zz4 * np.nan_to_num(
+                                spherical_jn(L, 1j * bb4 * rr * rl),
+                                nan=0.0,
+                                posinf=0.0,
+                                neginf=0.0) * np.nan_to_num(
+                                    np.exp(-aa4 * rr2 - gg4 * rl2),
+                                    nan=0.0,
+                                    posinf=0.0,
+                                    neginf=0.0)))
 
             # this function is high unstable for large r (it gives NaN but it should give 0.)
-            return np.nan_to_num(np.real(-V1 - V234))
+            return np.nan_to_num(np.real(V1 - V234))
 
         def pot_local(r, argv):
             r2 = r**2
@@ -395,16 +392,19 @@ for Ncore in range(16, 20):
                 energiesg = (valg[zg])
 
             ene_omega_Kex.append(energiesg[0])
-            print("nu: " + str(np.round(nu, 5)) + "  states: " + str(i) +
-                  "  Energies(g): " + str(energiesg))
+            print('A = %d  L = %2.2f  ' % (Ncore,
+                                           Lamb) + "  E: " + str(energiesg))
             if energiesg[0] < 0:
                 print('  A  Lambda        a    B(A+1)\n%3d %7.2f %8.4f %9.4f' %
                       (Ncore, Lamb, coreosci, np.real(energiesg[0])))
                 stable = True
+                break
         if stable != True:
             Lc.append([Ncore, Lamb])
             continue
         else:
-            Lamb *= 1.2
-
+            Lamb += dL
+            if Lamb > Lmax:
+                print(Lc)
+                exit()
     print(Lc)

LECs_interpolation_constr.py
@@ -1,22 +1,11 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from numpy.polynomial.polynomial import polyval, polyder
 
 
-def polinv(x, a, b, c, d):
-    return a + b / x + c / x**2 + d / x**3
-
-
-def pol(x, a, b, c, d):
-    return a + b * x + c * x**2 + d * x**3
-
-
-def pol4(x, a, b, c, d, e):
-    return a + b * x + c * x**2 + d * x**3 + e * x**4
-
-
-def return_val(lamb, data, var, rang=[0.1, 20], speculative=False):
+def return_val(lamb, data, var, polord=3, plot=False):
 
     L = C = D = AB = ABC = ABCD = ABCDE = ABCDEF = []
 
@@ -213,7 +202,7 @@ def return_val(lamb, data, var, rang=[0.1, 20], speculative=False):
         return 1
 
     X = np.array(L).astype(np.double)
-    fun = polinv
+
     if (var == "AB"):
         Y = np.array(AB).astype(np.double)
     elif (var == "ABC"):
@@ -226,10 +215,8 @@ def return_val(lamb, data, var, rang=[0.1, 20], speculative=False):
         Y = np.array(ABCDEF).astype(np.double)
     elif (var == "C"):
         Y = np.array(Cc).astype(np.double)
-        fun = pol
     elif (var == "D"):
         Y = np.array(Dd).astype(np.double)
-        fun = pol4
     else:
         print(str(var) + " is not a valid observable or lec.")
         print(" >> You can ask for C D AB ABC ABCD ABCDE or ABCDEF")
@@ -240,25 +227,42 @@ def return_val(lamb, data, var, rang=[0.1, 20], speculative=False):
         print("Sorry, we dont have the data you are looking for.")
         return 1
 
-    def funct(x, pars):
-        a = pars
-        return polyval(x, a)
-
     def residt(pars):
         return ((Y - polyval(X, pars))**2).sum()
 
     def constrt(pars):
-        return -polyval(rang, polyder(pars, m=1))
+        return np.sign(Y[-1]) * polyval(lamb, polyder(pars, m=1))
 
     con1t = {'type': 'ineq', 'fun': constrt}
     rest = minimize(
-        residt, [3, 5, 1],
+        residt,
+        np.random.rand(polord),
         method='cobyla',
         options={'maxiter': 50000},
         constraints=con1t)
 
+    fitdat = polyval(lamb, rest.x)
+
     plt.plot(X[mask], Y[mask], 'ko', label="data")
-    plt.plot(rang, polyval(rang, rest.x), label='fit')
-    plt.show()
-    exit()
-    return f2(lamb)
+    plt.plot(lamb, fitdat, label='fit')
+    plt.legend(loc='best', numpoints=1, fontsize=12)
+    plt.xlabel(r'$\Lambda\,[fm^{-1}]$', fontsize=12)
+    plt.ylabel(r'$%s\,[MeV]$' % var, fontsize=12)
+    strFile = 'Le%s_fit.pdf' % var
+    if os.path.isfile(strFile):
+        os.remove(strFile)
+    plt.savefig(strFile)
+    strFile = 'Le%s_fit.dat' % var
+    if os.path.isfile(strFile):
+        os.remove(strFile)
+    s = ''
+    for n in range(len(lamb)):
+        s += '%4.4f   %12.4f\n' % (float(lamb[n]), float(fitdat[n]))
+    with open(strFile, 'a') as outfile:
+        outfile.write(s)
+
+    if plot:
+        plt.show()
+    plt.clf()
+
+    return rest

the_parameter_a.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
@@ -8,11 +9,20 @@ from matplotlib.ticker import FormatStrFormatter
 from matplotlib.collections import LineCollection
 
 
-def fita(AA, plot=False):
+def fita(AA, order=2, orderp=1, plot=False):
 
-    Amax = 70
-    order = 5
-    Arange = np.arange(3, Amax)
+    if orderp >= order:
+        print(
+            'purely positive polynomial selected, so you are deprived of\n all the \"negative benefits!\" '
+        )
+        ordern = 0
+    else:
+        ordern = order - orderp
+
+    print('order of all powers          = %d' % order)
+    print('order of the negative powers = %d' % ordern)
+    print('order of the positive powers = %d' % orderp)
+    Arange = np.array(AA).astype(float)
 
     def func(x, pars):
         a, b, c = pars
@@ -22,19 +32,35 @@ def fita(AA, plot=False):
         return ((yy - func(xx, pars))**2).sum()
 
     def constr(pars):
-        #print(np.gradient(func(xx, pars)))
         return np.gradient(func(xx, pars))
 
     def funct(x, pars):
         a = pars
-        return polyval(x, a)
+        a[1] = 0
+        if orderp > 0:
+            a[ordern + 1] = 0
+            return a[0] + x**(-ordern - 1) * polyval(
+                x, a[1:ordern + 1]) + polyval(x, a[ordern + 1:])
+        else:
+            return a[0] + x**(-ordern - 1) * polyval(x, a[1:ordern + 1])
+
+    def residnp(pars):
+        return ((yy - funct(xx, pars))**2).sum()
+
+    def constrnp(pars):
+        if orderp > 0:
+            return (-1) * Arange**(-ordern - 1) * polyval(
+                Arange, polyder(pars[1:ordern + 1], m=1)) + polyval(
+                    Arange, polyder(pars[ordern + 1:], m=1))
+        else:
+            return (-1) * Arange**(-ordern - 1) * polyval(
+                Arange, polyder(pars[1:ordern + 1], m=1))
 
     def residt(pars):
         return ((yy - polyval(xx, pars))**2).sum()
 
     def constrt(pars):
-        #print(polyval(xx, np.polyder(pars)))
-        return polyval(Arange, polyder(pars, m=2))
+        return polyval(Arange, polyder(pars, m=1))
 
     colors = [
         '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',
@@ -49,49 +75,72 @@ def fita(AA, plot=False):
     Rcore = [float(line.strip().split()[5]) for line in data_rad]
     anz = len(Acore)
 
-    xx = Acore[2:]
-    yy = acrit[2:]
+    xx = np.array(Acore).astype(float)
+    yy = np.array(acrit).astype(float)
 
-    con1 = {'type': 'ineq', 'fun': constr}
-    res = minimize(
-        resid, [.3, 1., 1.],
+    con1np = {'type': 'ineq', 'fun': constrnp}
+    resnp = minimize(
+        residnp,
+        np.random.rand(order + 1),
         method='cobyla',
         options={'maxiter': 50000},
-        constraints=con1)
+        constraints=con1np)
+
+    print(resnp)
 
     con1t = {'type': 'ineq', 'fun': constrt}
     rest = minimize(
-        residt, [.3, 1, 5, 3, 5, 1],
+        residt,
+        np.random.rand(order),
         method='cobyla',
         options={'maxiter': 50000},
         constraints=con1t)
 
-    #print(res)
-    #print(rest)
+    apoly = polyval(Arange, rest.x)
+    apolyn = funct(Arange, resnp.x)
 
-    a1 = func(AA, res.x)
-    #print('a(%d) = %4.4f fm^-2' % (AA, a1))
-    a2 = polyval(AA, rest.x)
-
-    #print('a(%d) = %4.4f fm^-2' % (AA, a2))
+    heuriger = 1. / 2.
 
     def acritf(aa):
-        rr = Rcore[aa - 2] if (aa < 7) else 3.4
-        return rr**(-2) * 1.5 * (aa - 1) * (aa / (3 * aa - 2))**(1.5)
 
-    ame = acritf(AA)
+        zet = Rcore[-1] / Acore[-1]**(1. / 3.)
 
+        rr = Rcore[aa - 2] if (aa < 7) else zet * aa**(1. / 3.)
+
+        # return result in volume approximation for core numbers for which no
+        # microscopic data is available
+        a = heuriger * rr**(-2) * 1.5 * (aa - 1)**2 / aa if (
+            aa > 6) else acrit[aa - 2]
+        return a
+
+    ame = [acritf(int(aa)) for aa in Arange]
+
+    f = plt.figure(figsize=(10, 4))
+    f.suptitle(
+        r'$\kappa=\langle rms_{A=6}\rangle\cdot 6^{-1/3}=%4.2f$' %
+        (Rcore[-1] / Acore[-1]**(1. / 3.)),
+        fontsize=14)
+    ax1 = f.add_subplot(111)
+    ax1.set_xlabel(r'$A$', fontsize=12)
+    ax1.set_ylabel(r'core oscillator $a [fm^{-2}]$', fontsize=12)
+    #ax2 = f.add_subplot(122)
+    #ax1.set_ylim( [np.min(funct(Arange, resnp.x)),          np.max(funct(Arange, resnp.x))])
+    #ax1.plot(Arange, funct(Arange, resnp.x), label=r'$f(x)=\sum p_ix^i+n_ix^{-i}$')
+    ax1.plot(xx, yy, 'ro', label=r'SVM data')
+    #        ax1.plot(Arange, polyval(Arange, rest.x), label=r'$f(x)=\sum p_ix^i$')
+    ax1.plot(
+        Arange, [acritf(int(aa)) for aa in Arange],
+        label=r'$\kappa\cdot\frac{3\,(A-1)^2}{2\,A^{5/3}}$')
+    ax1.legend(loc=0, fontsize=14)
+    #ax2.plot(Arange, constrt(rest.x), label=r'slope$+$')
+    #ax2.plot(Arange, constrt(resnp.x), label=r'slope$\pm$')
+    #ax2.legend(loc=0)
+    strFile = 'a_para_fit.pdf'
+    if os.path.isfile(strFile):
+        os.remove(strFile)
+    plt.savefig(strFile)
     if plot:
-        f = plt.figure(figsize=(10, 4))
-        ax1 = f.add_subplot(121)
-        ax2 = f.add_subplot(122)
-        ax1.plot(xx, yy, 'ro', label='data')
-        ax1.plot(Arange, polyval(Arange, rest.x), label='fit')
-        ax1.plot(Arange, func(Arange, res.x), label='fit*')
-        ax1.plot(Arange, [acritf(aa) for aa in Arange], label='Osci r2')
-        ax1.legend(loc=0)
-        ax2.plot(Arange, constrt(rest.x), label='slope')
-        ax2.legend(loc=0)
         plt.show()
-        exit()
-    return a2  #ame
+    plt.clf()
+
+    return apolyn, apoly, ame